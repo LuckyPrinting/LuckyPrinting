04bee617c24b0079f0b1ddc3b5893366ee9cfb5c
Chen yg <yanguangchen@outlook.com>
2024-07-09 17:37:40 +0800

    commit new video instead of spline

--- src/spline.js
+++ src/spline.js
@@ -1,8 +1,16 @@
 
 import Spline from '@splinetool/react-spline';
-
+import mainVideo from './Assets/mainVideo.mp4';
 export default function SplineElement() {
   return (
-    <Spline scene="https://prod.spline.design/gEjwhpj6dNDva5Kh/scene.splinecode" />
+    <video
+    src={mainVideo}
+    style={{overflow: "hidden", objectFit: "scale-down"}}
+    autoPlay
+    loop
+    muted
+    playsInline
+    />  
+    // <Spline scene="https://prod.spline.design/gEjwhpj6dNDva5Kh/scene.splinecode" />
   );
 }
