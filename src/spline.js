6e8fe2ed7e83c8d21d5b94600f135ad525d6828f
Yanguangchen <yanguangchen@outlook.com>
2024-07-10 15:24:12 +0800

    CHANGE BACK TO SPLINME

--- src/spline.js
+++ src/spline.js
@@ -1,16 +1,8 @@
 
-// import Spline from '@splinetool/react-spline';
-import mainVideo from './Assets/MainVideo.m4v';
+import Spline from '@splinetool/react-spline';
+// import mainVideo from './Assets/MainVideo.m4v';
 export default function SplineElement() {
   return (
-    <video
-    src={mainVideo}
-    style={{overflow: "none", objectFit: "cover", width: "100%", height: "100%"}}
-    autoPlay
-    loop
-    muted
-    playsInline
-    />  
-    // <Spline scene="https://prod.spline.design/gEjwhpj6dNDva5Kh/scene.splinecode" />
+    <Spline scene="https://prod.spline.design/gEjwhpj6dNDva5Kh/scene.splinecode" />
   );
 }
