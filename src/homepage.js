7dc47f90801850a81063506ba8955399718c941e
Yanguangchen <yanguangchen@outlook.com>
2024-07-15 12:16:15 +0800

    c

--- src/homepage.js
+++ src/homepage.js
@@ -1,13 +1,14 @@
 import React from "react";
 import SpliceElement from "./splice";
-import GridContainer from "./gridContainer";
 import Services from "./Services";
 import Contactparent from "./contactParent";
+import LuckyPrintingSection from "./about";
 
 function Homepage() {
   return (
     <div>
       <SpliceElement />
+      <LuckyPrintingSection />
       <Services />
       <Contactparent />
     </div>
